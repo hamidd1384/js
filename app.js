563b330af3a6e6a38ae2ad8df182cd362dce5c25
Hamid-dehmardh84 <hdhmrdh30@gmail.com>
2026-02-06 10:02:51 +0330

    test js

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
-const arr = new Array (5, "hello", 6, "aomid" , 8, 9, 10);
+// const arr = new Array (5, "hello", 6, "aomid" , 8, 9, 10);
 
-let x;
-arr[2] = 7;
-console.log(arr);
+// let x;
+// arr[2] = 7;
+// console.log(arr);
+
+
+
+
+console.log('hello hamid')
